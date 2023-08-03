17018ae6d06eac0a21f81f47ff689eb1b9d89408
root <root@DESKTOP-Q06KE0B>
2023-08-03 16:35:01 -0500

    Address Detect

--- Bash-Scripts/Address_Detect.sh
+++ Bash-Scripts/Address_Detect.sh
@@ -8,4 +8,13 @@ if test $arp_value == "0"; then
 	echo "All dependencies are installed"
 	read -p "1. MAC Address  2. IP Address  3. MAC & IP Address: " option
 	echo $option
+	if test $option == "1"; then
+		arp-scan --localnet | grep -A 10 "Starting" | awk 'NF && $2 ~ /^[0-9]/{print $2}' > /dev/null
+	fi
+	if test $option == "2"; then
+		arp-scan --localnet | grep -A 10 "Starting" | awk 'NF && $2 ~ /^[0-9]/{print $1}' > /dev/null
+        fi
+	if test $option == "3"; then
+		arp-scan --localnet | grep -A 10 "Starting" | awk 'NF && $2 ~ /^[0-9]/' > /dev/null
+        fi
 fi
